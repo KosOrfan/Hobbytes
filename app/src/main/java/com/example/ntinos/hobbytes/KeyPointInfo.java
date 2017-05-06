e4fdac0563c6c7df535bb918753c88a32b00e8b4
KostOrv <konst.orfan@gmail.com>
2017-05-07 02:59:44 +0300

    Change UI. Implement image picker in RegisterPoint class. Create CheckedUtils class for exception handling. IMPORTANT: Upgraded whole project from Java 7 ver. to Java 8. (Must build whole project before 1st run).

--- app/src/main/java/com/example/ntinos/hobbytes/KeyPointInfo.java
+++ app/src/main/java/com/example/ntinos/hobbytes/KeyPointInfo.java
@@ -1,10 +1,9 @@
 package com.example.ntinos.hobbytes;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.widget.ImageView;
 import android.widget.TextView;
-
 
 import static com.example.ntinos.hobbytes.MapsActivity.EXTRA_NAME;
 
@@ -18,13 +17,19 @@ public class KeyPointInfo extends AppCompatActivity {
         String KpName = getIntent().getStringExtra(EXTRA_NAME);
         String KpSnip = getIntent().getExtras().getString("Snippet");
 
-        TextView KeyPointName = (TextView)findViewById(R.id.KpInformation);
+        //Code to receive Image from Extras
+        //TODO: Διορθωση τροπου μεταφορας της εικονας
+        /*String imagePath = getIntent().getExtras().getString("Image");
+        File filePath = getFileStreamPath(imagePath);
+        Drawable image = Drawable.createFromPath(filePath.toString());*/
+
+        TextView KeyPointName = (TextView) findViewById(R.id.KpInformation);
         KeyPointName.setText(KpSnip);
-        TextView KeyPointSnip = (TextView)findViewById(R.id.KpName);
+        TextView KeyPointSnip = (TextView) findViewById(R.id.KpName);
         KeyPointSnip.setText(KpName);
 
         ImageView KeyPointPic = (ImageView) findViewById(R.id.imageView2);
-        KeyPointPic.setImageResource(R.drawable.sea);
+        //KeyPointPic.setImageDrawable(image);
 
 
     }
