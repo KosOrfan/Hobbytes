ff319ee6a09d993ef1d6b32cdcc79ae90ac057f8
niephox <rezutasos@gmail.com>
2017-05-07 12:35:22 +0300

    Google Location API client

--- app/src/main/java/com/example/ntinos/hobbytes/RegisterPoint.java
+++ app/src/main/java/com/example/ntinos/hobbytes/RegisterPoint.java
@@ -3,9 +3,14 @@ package com.example.ntinos.hobbytes;
 import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
+import android.content.pm.PackageManager;
 import android.graphics.drawable.BitmapDrawable;
+import android.location.Location;
 import android.os.Bundle;
 import android.provider.MediaStore;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.support.v4.app.ActivityCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
@@ -13,16 +18,23 @@ import android.widget.EditText;
 import android.widget.ImageButton;
 import android.widget.ImageView;
 
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.location.LocationServices;
 import com.google.android.gms.maps.model.LatLng;
 
-public class RegisterPoint extends AppCompatActivity {
+public class RegisterPoint extends AppCompatActivity implements GoogleApiClient.ConnectionCallbacks, GoogleApiClient.OnConnectionFailedListener {
 
 
     public static boolean registerPointOpened = false;
     private static int RESULT_LOAD_IMAGE = 1;
     private final String FILEPATH = "FilePath";
-
+    GoogleApiClient mGoogleApiClient;
     String path;
+    Location mLastLocation;
+    LatLng mLocation;
+    Double mLat;
+    Double mLong ;
 
     SharedPreferences prefs;
 
@@ -37,6 +49,17 @@ public class RegisterPoint extends AppCompatActivity {
         final EditText nameText = (EditText) findViewById(R.id.nameText);
         final EditText infoText = (EditText) findViewById(R.id.infoText);
         final ImageView imageView = (ImageView) findViewById(R.id.imageView);
+
+        //Connecting to googleplay services
+
+        if (mGoogleApiClient == null) {
+            mGoogleApiClient = new GoogleApiClient.Builder(this)
+                    .addConnectionCallbacks(this)
+                    .addOnConnectionFailedListener(this)
+                    .addApi(LocationServices.API)
+                    .build();
+        }
+
 
         //Image button event
         imageBtn.setOnClickListener(new View.OnClickListener() {
@@ -58,9 +81,7 @@ public class RegisterPoint extends AppCompatActivity {
                 Intent intent = new Intent(RegisterPoint.this, MapsActivity.class);
 
                 //Get marker Info
-                LatLng mLocation = new LatLng(41, 23);
-                Double mLang = 33.1;
-                Double mLong = 20.1;
+
 
                 registerPointOpened = true;
 
@@ -69,7 +90,7 @@ public class RegisterPoint extends AppCompatActivity {
                 intent.putExtra("Info", infoText.getText().toString());
                 intent.putExtra("location", mLocation);
                 intent.putExtra("mlong", mLong);
-                intent.putExtra("mlang", mLang);
+                intent.putExtra("mlang", mLat);
 
                 //Code to transfer image as Extra
                 //TODO: Ορισμος τροπου μεταφορας της εικονας
@@ -87,6 +108,8 @@ public class RegisterPoint extends AppCompatActivity {
                 //    ioe.printStackTrace();
                 //}
                 //intent.putExtra("Image", imagePath);
+
+                //TODO: Get current location and transfer LATLANG as extra.
 
 
                 startActivity(intent);
@@ -112,4 +135,49 @@ public class RegisterPoint extends AppCompatActivity {
             }
         }
     }
+
+    //GOOGLE LOCATION API FUNC
+
+    @Override
+    public void onConnected(@Nullable Bundle bundle) {
+        //Call get location method
+        if (ActivityCompat.checkSelfPermission(this, android.Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, android.Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            // TODO: Consider calling
+            //    ActivityCompat#requestPermissions
+            // here to request the missing permissions, and then overriding
+            //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
+            //                                          int[] grantResults)
+            // to handle the case where the user grants the permission. See the documentation
+            // for ActivityCompat#requestPermissions for more details.
+            return;
+        }
+        mLastLocation = LocationServices.FusedLocationApi.getLastLocation(mGoogleApiClient);
+        if (mLastLocation != null) {
+            mLat = mLastLocation.getLatitude();
+            mLong = mLastLocation.getLongitude();
+
+        }
+
+    }
+
+    @Override
+    public void onConnectionSuspended(int i) {
+
+    }
+
+    @Override
+    public void onConnectionFailed(@NonNull ConnectionResult connectionResult) {
+
+    }
+
+    protected void onStart() {
+        mGoogleApiClient.connect();
+        super.onStart();
+    }
+
+    protected void onStop() {
+        mGoogleApiClient.disconnect();
+        super.onStop();
+    }
+
 }
